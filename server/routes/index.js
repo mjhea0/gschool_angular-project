d77a3adbbe4ae9a368adb261b84ab4fe8f5c554f
ethan <ethanmannette@gmail.com>
2015-10-05 14:44:00 -0600

    Added new get request for homepage after login

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var passport = require('passport');
+var Account = require('../../models/account');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Login' });
 });
+
+router.get('/home', function(req, res, next) {
+  res.render('', {title: 'Homepage'});
+});
+
 
 module.exports = router;
