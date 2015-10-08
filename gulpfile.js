5d5c257161d00bdac406bae353ad21cb504b5b28
ethan <ethanmannette@gmail.com>
2015-10-08 16:20:19 -0600

    changed localhost for server

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,7 @@ gulp.task('lint', function() {
 
 gulp.task('browser-sync', ['nodemon'], function(done) {
   browserSync({
-    proxy: "localhost:3000",  // local node app address
+    proxy: "http://127.0.0.1:3000",  // local node app address
     port: 5000,  // use *different* port than above
     notify: true
   }, done);
