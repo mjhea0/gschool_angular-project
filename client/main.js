9ce344b196629d63ff6e82ded3e9e2ca7a6dbe7c
ethan <ethanmannette@gmail.com>
2015-10-13 10:19:49 -0600

    commented out function which checks if user is logged in on page refresh. Was causing problems. Plan to refactor

--- client/main.js
+++ client/main.js
@@ -2,18 +2,30 @@ var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.config(function ($routeProvider) {
   $routeProvider
-    .when('/', {templateUrl: '/partials/home.html', controller: 'loginController'})
-    .when('/register', {controller: 'registerController'})
-    .when('/logout', {controller: 'logoutController'})
-    .when('/main', {templateUrl: '/partials/main.html'});
+    .when('/', {
+      templateUrl: '/partials/home.html',
+      controller: 'loginController',
+      access: {restricted: false}
+    })
+    .when('/logout', {
+      controller: 'logoutController',
+      access: {restricted: false}
+    })
+    .when('/main', {
+      templateUrl: '/partials/main.html',
+      controller: 'githubController',
+      access: {restricted: true}
+    })
+    .otherwise({redirectTo: '/'});
 });
 
-myApp.run(function ($rootScope, $location, $route, AuthService) {
-  $rootScope.$on('$routeChangeStart', function (event, next, current) {
-    if (AuthService.isLoggedIn() === false) {
-      $location.path('/');
-    }
-  });
+// myApp.run(function ($rootScope, $location, $route, AuthService) {
+//   $rootScope.$on('$routeChangeStart', function (event, next, current) {
+//     console.log(AuthService.getUserStatus());
+//     if (next.access.restricted && !AuthService.getUserStatus()) {
+//       $location.path('/');
+//     }
+//   });
 
 
-});
+// });
