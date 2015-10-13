35cffee0d23fc1bd4594adb6b60923a4eb28cabb
ethan <ethanmannette@gmail.com>
2015-10-13 10:23:30 -0600

    refactored routesand seperated api callback from main routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,46 +1,11 @@
-var express = require('express'),
-    router = express.Router(),
-    passport = require('passport');
-    User = require('../models/user.js');
-
-
-router.post('/register', function(req, res) {
-  User.register(new User({ username: req.body.username }), req.body.password, function(err, account) {
-    if (err) {
-      return res.status(500).json({err: err});
-    }
-    passport.authenticate('local')(req, res, function () {
-      return res.status(200).json({status: 'Registration successful!'});
-    });
-  });
-});
-
-router.post('/login', function(req, res, next) {
-  passport.authenticate('local', function(err, user, info) {
-    if (err) { return next(err); }
-    if (!user) {
-      return res.status(401).json({err: info});
-    }
-    req.logIn(user, function(err) {
-      if (err) {
-        return res.status(500).json({err: 'Could not log in user'});
-      }
-      res.status(200).json({status: 'Login successful!'});
-    });
-  })(req, res, next);
-});
-
-router.get('/logout', function(req, res) {
-  req.logout();
-  res.status(200).json({status: 'Bye!'});
-});
-
+var express = require('express');
+var router = express.Router();
 var passportGithub = require('../auth/github');
 
-router.get('/auth/github', passportGithub.authenticate('github', { scope: [ 'user:email' ] }));
+router.get('/auth/github', passportGithub.authenticate('github', { scope: [ 'user, public_repo' ] }));
 
 router.get('/auth/github/callback',
-  passportGithub.authenticate('github', { failureRedirect: '/' }),
+  passportGithub.authenticate('github', { failureRedirect: '/', successRedirect: '/#/main' }),
   function(req, res) {
     // Successful authentication
     // console.log(req);
